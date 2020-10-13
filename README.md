df65e7d8ce8e324123093eaeb7cc07031988a8f8
Meghna <meghnaparsan@gmail.com>
2020-10-13 13:19:36 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather Application
+
+This is a Weather Application developed using Flutter. 
+
+## To run the project
+- Clone it with the url [https://github.com/meghnaparsan/weather_app.git](https://github.com/meghnaparsan/weather_app.git)
+- Go to lib/main.dart -> Click "Press Dependencies"
+- Create an account in [https://openweathermap.org/](https://openweathermap.org/). Get an API key.
+- Go to lib/services/weather.dart. Change the API Key.
+- Done! The App would be ready to run on any Physical Device / Emulator
+ 
+Reference: The Complete 2020 Flutter Development Bootcamp with Dart
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:weather_app/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:weather_app/services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+    if (weatherData == null) {
+      SystemNavigator.pop();
+    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return LocationScreen(weatherData);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
